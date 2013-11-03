9e5da4c1f653858d5e9bfe3fcb1d4fd1bff61865
Jason Roth <jasonmichaelroth@gmail.com>
2013-11-03 12:23:17 -0700

    refactor rates data structure

--- lib/converts_rates.rb
+++ lib/converts_rates.rb
@@ -10,27 +10,49 @@ class ConvertsRates
   end
 
   def convert(amount, from, to)
-    amount.to_d * conversion(from, to)
+    amount.to_d * conversion_rate(from, to)
   rescue NoConversion
     :no_conversion
   end
 
   private
 
-  def populate_rates(rates)
-    rates.map do |rate|
-      from = rate.fetch(:from) { :NA }.to_sym
-      to = rate.fetch(:to) { :NA }.to_sym
-      conversion = rate.fetch(:conversion) { 1.0 }.to_f
-      [
-        {[from, to] => conversion},
-        {[to, from] => (1.0 / conversion)}
-      ]
-    end.flatten.reduce({}){|m,r| m.merge(r)}
+  def populate_rates(rates_data)
+    rates_data.reduce([]) do |rates, rate_data|
+      rate = Rate.new.set_data(rate_data)
+      rates << rate
+      rates << rate.inverse
+      rates
+    end
   end
 
-  def conversion(from, to)
-    @rates.fetch([from, to]) { raise NoConversion, "no conversion from #{from} to #{to}" }
+  def conversion_rate(from, to)
+    rate =
+      @rates.find do |rate|
+        rate.converts?(from, to)
+      end || raise(NoConversion, "no conversion from #{from} to #{to}")
+    rate.conversion
+  end
+
+  Rate = Struct.new(:from, :to, :conversion) do
+    def set_data(data)
+      self[:from] = data.fetch(:from).to_sym
+      self[:to] = data.fetch(:to).to_sym
+      self[:conversion] = data.fetch(:conversion).to_d
+      self
+    end
+
+    def inverse
+      Rate.new.set_data({
+        from: to,
+        to: from,
+        conversion: (1.0.to_d / conversion)
+      })
+    end
+
+    def converts?(from, to)
+      self[:from] == from && self[:to] == to
+    end
   end
 
 end
