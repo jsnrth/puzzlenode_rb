5fb7c6197e01031951613c53194cd77f3ff9b9be
Jason Roth <jasonmichaelroth@gmail.com>
2013-11-03 13:47:35 -0700

    refactor

--- lib/converts_rates.rb
+++ lib/converts_rates.rb
@@ -22,9 +22,7 @@ class ConvertsRates
   def populate_rates(rates_data)
     rates_data.reduce([]) do |rates, rate_data|
       rate = Rate.new.set_data(rate_data)
-      rates << rate
-      rates << rate.inverse
-      rates
+      rates << rate << rate.inverse
     end
   end
 
@@ -37,33 +35,30 @@ class ConvertsRates
       [rate, new_rates]
     else
       froms = all_rates.select {|frate| frate.from == from}
-      if froms.empty?
-        raise NoConversion, "no conversion from #{from} to #{to}"
-      end
-
-      froms.each do |from_rate|
-        tos = all_rates.select {|trate| trate.from == from_rate.to}
-        if tos.empty?
-          raise NoConversion, "no conversion from #{from} to #{to}"
-        end
-
-        tos.each do |to_rate|
-          new_rate = Rate.new.set_data({
-            from: from_rate.from,
-            to: to_rate.to,
-            conversion: (from_rate.conversion * to_rate.conversion)
-          })
-
-          is_dup = new_rate.from == new_rate.to
-          exists = all_rates.find{|r| new_rate.from == r.from && new_rate.to == r.to }
-
-          if !is_dup && !exists
-            new_rates << new_rate
+      if froms.any?
+        froms.each do |from_rate|
+          tos = all_rates.select {|trate| trate.from == from_rate.to}
+          if tos.any?
+            tos.each do |to_rate|
+              new_from = from_rate.from
+              new_to = to_rate.to
+              new_conversion = from_rate.conversion * to_rate.conversion
+              is_dup = new_from == new_to
+              exists = all_rates.find {|r| new_from == r.from && new_to == r.to }
+              if !is_dup && !exists
+                new_rates << Rate.new.set_data({
+                  from: new_from,
+                  to: new_to,
+                  conversion: new_conversion
+                })
+              end
+            end
           end
         end
       end
 
-      if new_rates.count > new_rates_count
+      any_new_rates = new_rates.count > new_rates_count
+      if any_new_rates
         derive_conversion_rate(rates, from, to, new_rates)
       else
         raise NoConversion, "no conversion from #{from} to #{to}"
